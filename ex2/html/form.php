285f328e89e6a4fb4f0682e7dc2b4f8ba6f398f0
nammulovett <nammmu@gmail.com>
2022-11-06 13:04:33 +0100

    dem

--- ex2/html/form.php
+++ ex2/html/form.php
@@ -1,57 +1,87 @@
 <!DOCTYPE html>
 <html lang="es">
-
-<head>
+  <head>
     <meta charset="UTF-8" />
+    <!-- charset  unicode, todos los idiomas, emojis... -->
+    <meta name="description" content="This is my first webpage ... " />
+    <!-- en name pones el nombre del metadato. y en content el metadato: En desctiption es una descripción en lenguaje natural  -->
+    <meta name="keywords" content="coding, html, programming" />
+    <!-- keywords ayuda a posicionarnos frente a los motores de búsqueda -->
+    <meta name="author" content="Manuel Gómez Lora" />
+    <!-- Autor de la página web -->
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- Para dar soporte a las versiones antiguas de IE -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-</head>
+    <!-- El conjunto de cosas que se muestra en pantalla, es decir si no se pone, las páginas responsives no se muestran, es la referencia de estas-->
+    <meta name="generator" content="Programa" />
+    <!-- Si nuestra página web ha sido creada de forma automáticamente con un programa -->
+    <link rel="stylesheet" href="style.css" />
+    <!-- Señalar contenido que tiene relación con el html de la web, tienes que poner en rel la palabra cable -->
+    <link rel="icon" href="img/favicon.ico" />
+    <!-- Favicon de la web -->
+    <title>FORM 2</title>
+  </head>
 
-<body>
+  <body>
+    <section>
+      <nav>
+        <a href="../index.html">HOME</a> |
+        <a href="./html/tournaments.html">TOURNAMENTS</a> |
+        <a href="./html/events.html">EVENTS</a> |
+        <a href="./html/media.html">MEDIA</a> |
+        <a href="./html/aboutus.html">ABOUT US</a> | <a href="#">FORM 1</a> |
+        <a href="form2.html">FORM 2</a>
+      </nav>
+    </section>
 
     <?php
     if (!isset($_GET["name"])) {
     ?>
-        <form action="#" method="GET">
-            <fieldset>
-                <legend>Datos Personales</legend>
-                <p><label for="name">Nombre: </label>
-                    <input type="text" name="name" id="name">
-                </p>
-                <p><label for="surname">Primer apellido:</label>
-                    <input type="text" name="surname" id="surname">
-                </p>
-                <p><label for="surname2">Segundo apellido:</label>
-                    <input type="text" name="surname2" id="surname2">
-                </p>
-                <p><label for="adress">Dirección:</label>
-                    <input type="text" name="adress" id="adress">
-                </p>
-                <p><label for="email">Email</label>
-                    <input type="email" name="email" id="email">
-                </p>
-                <p><label for="date">fecha de nacimiento</label>
-                    <input type="date" name="date" id="date">
-                </p>
-            </fieldset>
-            <fieldset>
-                <legend>Otros datos</legend>
-            </fieldset>
-            <p>
-                <label for="tlf"> Teléfono</label>
-                <input type="text" name="tlf" id="tlf" />
-            </p>
-            <p>
-                <label for="color"> Color Favorito</label>
-                <input type="color" name="color" id="color" value="#CB1363" />
-            </p>
-            <p>
-                <label for="web"> Página inicio</label>
-                <input type="web" name="web" id="web" />
-            </p>
-            <p><input type="submit" value="enviar"></p>
-        </form>
+    <form action="#" method="GET">
+      <fieldset>
+        <legend>Datos Personales</legend>
+        <p>
+          <label for="name">Nombre: </label>
+          <input type="text" name="name" id="name" />
+        </p>
+        <p>
+          <label for="surname">Primer apellido:</label>
+          <input type="text" name="surname" id="surname" />
+        </p>
+        <p>
+          <label for="surname2">Segundo apellido:</label>
+          <input type="text" name="surname2" id="surname2" />
+        </p>
+        <p>
+          <label for="adress">Dirección:</label>
+          <input type="text" name="adress" id="adress" />
+        </p>
+        <p>
+          <label for="email">Email</label>
+          <input type="email" name="email" id="email" />
+        </p>
+        <p>
+          <label for="date">fecha de nacimiento</label>
+          <input type="date" name="date" id="date" />
+        </p>
+      </fieldset>
+      <fieldset>
+        <legend>Otros datos</legend>
+      </fieldset>
+      <p>
+        <label for="tlf"> Teléfono</label>
+        <input type="text" name="tlf" id="tlf" />
+      </p>
+      <p>
+        <label for="color"> Color Favorito</label>
+        <input type="color" name="color" id="color" value="#CB1363" />
+      </p>
+      <p>
+        <label for="web"> Página inicio</label>
+        <input type="web" name="web" id="web" />
+      </p>
+      <p><input type="submit" value="enviar" /></p>
+    </form>
 
     <?php
     } else {
@@ -210,8 +240,5 @@
             <input type="submit" value="enviar" />
         </p>
     </form> -->
-
-
-</body>
-
+  </body>
 </html>
