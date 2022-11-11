c6ca0304a5f5d7712ba38afccc55396dead846a0
nammulovett <nammmu@gmail.com>
2022-11-11 12:48:03 +0100

    final version .php

--- ex2/form2.php
+++ ex2/form2.php
@@ -1,22 +1,49 @@
 <?php
 
 echo $_GET["radio"];
+echo "</br>";
 echo $_GET["name"];
+echo "</br>";
 echo $_GET["surname"];
+echo "</br>";
 echo $_GET["city"];
+echo "</br>";
 echo $_GET["region"];
-echo $GET["postCode"];
+echo "</br>";
+echo $_GET["postCode"];
+echo "</br>";
 echo $_GET["rcode"];
+echo "</br>";
 echo $_GET["milista"];
+echo "</br>";
 echo $_GET["subsCode"];
-echo $_GET["o1"];
-echo $_GET["o2"];
-echo $_GET["o3"];
-echo $_GET["o4"];
-echo $_GET["o5"];
+echo "</br>";
+
+if (!empty($_GET["o1"])) {
+    echo $_GET["o1"];
+}
+if (!empty($_GET["o2"])) {
+    echo $_GET["o2"];
+}
+if (!empty($_GET["o3"])) {
+    echo $_GET["o3"];
+}
+if (!empty($_GET["o4"])) {
+    echo $_GET["o4"];
+}
+if (!empty($_GET["o5"])) {
+    echo $_GET["o5"];
+}
+echo "</br>";
 echo $_GET["username"];
+echo "</br>";
 echo $_GET["passwd"];
+echo "</br>";
 echo $_GET["credCard"];
+echo "</br>";
 echo $_GET["cardNum"];
+echo "</br>";
 echo $_GET["disCode"];
+echo "</br>";
 echo $_GET["textarea"];
+echo "</br>";
