0f8a6bd48b36d8839f6bedab5de9a3cb39877276
nammulovett <nammmu@gmail.com>
2022-11-30 19:18:10 +0100

    .

--- ang/videoweb/script.js
+++ ang/videoweb/script.js
@@ -1,2 +1,7 @@
 const container = document.querySelector('.container');
 const toggle = document.querySelector('.toggle');
+
+container.addEventListener('click', () => {
+  container.classList.toggle('active');
+  toggle.classList.toggle('active');
+});
