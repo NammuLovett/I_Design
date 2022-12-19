4afebb9aeb1da1a3fbb8164e330b7fbe5908cbfa
nammulovett <nammmu@gmail.com>
2022-12-19 10:20:50 +0100

    .

--- ang/videoweb/script.js
+++ ang/videoweb/script.js
@@ -5,3 +5,22 @@ container.addEventListener('click', () => {
   container.classList.toggle('active');
   toggle.classList.toggle('active');
 });
+
+/* display block
+width 50px
+height 50px
+
+border box 
+margin 0
+padding 0
+
+contenedor líneas{
+  span
+  span
+  span  
+
+  display block
+  opacity 0.
+  transform origin
+}
+ */
